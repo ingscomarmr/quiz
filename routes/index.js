f019b28b586f1b7631b716d31379840353583163
ingscomarmr <ingscomarmr@gmail.com>
2015-07-14 21:24:16 -0500

    V3
    Parcial porque aun no corre

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+//para las preguntas
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz - Omar M.R' });
 });
+
+
+//para las preguntas 
+router.get('/quizes/question',quizController.questions);
+router.get('/quizes/answer',quizController.answer);
 
 module.exports = router;
